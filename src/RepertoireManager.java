9db7a2fc953b477cbdf14db465bf4737b1c46c6f
User <User@DESKTOP-M7IUU2A>
2022-03-10 02:38:04 +0900

    Replaced hello.java to RepertoireManager.java

---
+++ src/RepertoireManager.java
@@ -0,0 +1,12 @@
+public class RepertoireManager {
+
+	public static void main(String[] args) {
+
+		System.out.println("1. Add Repertoire");
+		System.out.println("2. Delete Repertoire");
+		System.out.println("3. Edit Repertoire");
+		System.out.println("4. View Repertoire");
+		System.out.println("5. Exit");
+
+	}
+}
